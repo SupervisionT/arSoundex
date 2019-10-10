cd60c6149f736b37f4a27a9bb153b919bf122f9a
Ahmed El Gabri <ahmed@gabri.me>
2019-10-10 21:25:57 +0200

    Update tests to test both generated bundles

--- test/index.js
+++ test/index.js
@@ -1,7 +1,13 @@
 import test from 'ava';
-import arSoundex from '../src';
+import arSoundexCJS from '../bundle';
+import arSoundexESM from '../bundle/index.esm';
 
-test('Make sure algorithm works as expected', t => {
-  t.is(arSoundex('ضربوه', 6, 1), 'x26700');
-  t.is(arSoundex('ضرب', 6, 1), 'x26700');
+test('Make sure function works as expected in CJS bundle', t => {
+  t.is(arSoundexCJS('ضربوه', 6, 1), 'x26700');
+  t.is(arSoundexCJS('ضرب', 6, 1), 'x26700');
+});
+
+test('Make sure function works as expected in ESM bundle', t => {
+  t.is(arSoundexESM('ضربوه', 6, 1), 'x26700');
+  t.is(arSoundexESM('ضرب', 6, 1), 'x26700');
 });
